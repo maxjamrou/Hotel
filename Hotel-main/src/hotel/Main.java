6caf1caa57f9c0b23b8f5d3fb43aaae0f0e29b0b
OwksCode <hak.kevin.4@gmail.com>
2026-05-14 16:48:52 +0200

    Add files via upload

---
+++ Hotel-main/src/hotel/Main.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import model.*;
+import vue.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Hotel hotel = new Hotel("Best hotel ever", "Evry");
+        VueHotel window = new VueHotel(hotel);
+        for(int i = 0; i<4; i++){
+            hotel.addClient(new Client("Personne", (i + ""), hotel));
+            if(i == 2 || i == 3){
+                hotel.addChambre(new Chambre(i, i, true, "Double", hotel));
+                
+            }
+            hotel.addChambre(new Chambre(i, i, false, "Simple", hotel));
+            hotel.addReservation(new Reservation(LocalDate.parse("2026-04-21"), LocalDate.parse("2026-05-21"), hotel, hotel.getChambres().get(i), hotel.getClients().get(i)));
+            hotel.addProduit(new Produit(("Produit " + i), (2 + i), (5.2 + i), hotel));
+        }
+        hotel.addSejour(new Sejour(hotel.getReservations().get(1), hotel));
+        hotel.addSejour(new Sejour(hotel.getReservations().get(2), hotel));
+        hotel.getSejours().get(1).getConsommationMinibar().addProduit(hotel.getProducts().get(0));
+        hotel.getSejours().get(1).getConsommationMinibar().addProduit(hotel.getProducts().get(1));
+        hotel.addSejour(new Sejour(hotel.getReservations().get(3), hotel));
+        hotel.getSejours().get(2).getConsommationMinibar().addProduit(hotel.getProducts().get(2));
+        hotel.getSejours().get(2).getConsommationMinibar().addProduit(hotel.getProducts().get(3));
+        System.out.println(hotel.getChambres().get(2).getFloor());
+    }
+}
---
+++ Hotel-main/src/hotel/controller/ControllerMenu.java
@@ -0,0 +1,77 @@
+package controller;
+
+import java.awt.Desktop;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+import javax.swing.*;
+import vue.VueGererChambre;
+import vue.VueGererClient;
+import vue.VueGererProduit;
+import vue.VueHotel;
+public class ControllerMenu implements ActionListener{
+    /**
+     * Attributs de ce qui peut changer
+     */
+    VueHotel vueHotel;
+    Vector<JPanel> listeActions;
+    /**
+     * Constructeur
+     */
+    public ControllerMenu(VueHotel vueHotel, Vector<JPanel> la){
+        this.vueHotel = vueHotel;
+        this.listeActions = la;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        this.vueHotel.getContentPane().removeAll();
+        switch (((JMenuItem)e.getSource()).getText()) {
+            case "Nouveau client":
+                this.vueHotel.getContentPane().add(this.listeActions.get(0));
+                break;
+            case "Gérer la liste des clients":
+                ((VueGererClient)this.listeActions.get(1)).refresh();
+                this.vueHotel.getContentPane().add(this.listeActions.get(1));
+                break;
+            case "Nouvelle chambre":
+                this.vueHotel.getContentPane().add(this.listeActions.get(2));
+                break;
+            case "Gérer la liste des chambres":
+                ((VueGererChambre)this.listeActions.get(3)).refresh();
+                this.vueHotel.getContentPane().add(this.listeActions.get(3));
+                break;
+            case "Nouvelle réservation":
+                this.vueHotel.getContentPane().add(this.listeActions.get(4));
+                break;
+            case "Gérer la liste des réservations":
+                this.vueHotel.getContentPane().add(this.listeActions.get(5));
+                break;
+            case "Gérer la liste des séjours":
+                this.vueHotel.getContentPane().add(this.listeActions.get(6));
+                break;
+            case "Nouveau produit":
+                this.vueHotel.getContentPane().add(this.listeActions.get(7));
+                break;
+            case "Gérer la liste des produits":
+                ((VueGererProduit)this.listeActions.get(8)).refresh();
+                this.vueHotel.getContentPane().add(this.listeActions.get(8));
+                System.out.println(this.vueHotel.getHotel().afficheAllProduit());
+                break;
+            case "Ouvrir l'UML":
+                System.out.println("test");
+                if (Desktop.isDesktopSupported()) {
+                    try {
+                        Desktop.getDesktop().open(new File("../../Hotelpdf.pdf"));
+                    } catch (IOException ex) {
+                        // no application registered for PDFs
+                    }
+                }   break;
+            default:
+                break;
+        }
+        this.vueHotel.repaint();
+        this.vueHotel.pack();
+    }
+}
